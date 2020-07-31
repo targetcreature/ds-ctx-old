d1b61553bd9841b30d0f3ffec3c2b3c75fbe5a4d
Craig Hildebrand <targetcreature@gmail.com>
2020-07-31 10:32:19 -0700

    use-immer

--- components/_provider.tsx
+++ components/_provider.tsx
@@ -1,22 +1,20 @@
-import produce, { castDraft, castImmutable, Draft } from 'immer'
-import { useState } from 'react'
-// import { useImmer } from 'use-immer'
+import { useImmer } from 'use-immer'
 import { ICTX } from '..'
 import { Init } from '../_types'
 
-type Dispatch<State> = (dispatch: (draft: Draft<State>) => void | State) => void
-type ImmerStore<State> = [State, Dispatch<State>]
+// type Dispatch<State> = (dispatch: (draft: Draft<State>) => void | State) => void
+// type ImmerStore<State> = [State, Dispatch<State>]
 
-export function useImmer<S = any>(initialValue: S): ImmerStore<S> {
-    const [state, updateState] = useState<S>(initialValue)
-    const dispatch = (updater: (draft: Draft<S>) => void) => {
-        const newState = produce(updater)(castImmutable(castDraft(state))) as S
-        updateState(newState)
-        // return newState
-    }
+// export function useImmer<S = any>(initialValue: S): ImmerStore<S> {
+//     const [state, updateState] = useState<S>(initialValue)
+//     const dispatch = (updater: (draft: Draft<S>) => void) => {
+//         const newState = produce(updater)(castImmutable(castDraft(state))) as S
+//         updateState(newState)
+//         // return newState
+//     }
 
-    return [state, dispatch]
-}
+//     return [state, dispatch]
+// }
 
 export const _provider = <T extends Init>(CTX: ICTX<T>) => ({ children }) => {
 
